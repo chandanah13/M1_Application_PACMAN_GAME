fb8c5f4c01be561dcf045e83ee89411bf56929e9
chandanah13 <82013605+chandanah13@users.noreply.github.com>
2021-11-23 23:49:52 +0530

    Update update test_operation.c

--- 3_Implementation/update test_operation.c
+++ 3_Implementation/update test_operation.c
@@ -1,131 +1,161 @@
-#include "unity.h"
-#include "operation.h"
-
-void test_display_board(void);
-void test_input(void);
-void test_winner(void);
-
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
+#include "header.h"
 
 
-int main()
+
+SDL_Rect cor={24,25};
+int quit=no;
+
+SDL_Surface *load_image( char filename[] )
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
+    //Temporary storage for the image that's loaded
+    SDL_Surface* loadedImage = NULL;
 
-/* Run Test functions */
-  RUN_TEST(test_display_board);
-  RUN_TEST( test_input );
-  RUN_TEST( test_winner);
+    //The optimized image that will be used
+    SDL_Surface* optimizedImage = NULL;
+    //Load the image
+    char file[]="sprites/";
+    strcat(file,filename);
 
-  /* Close the Unity Test Framework */
-  return UNITY_END();
-}
+    loadedImage = IMG_Load( file);
+    //If nothing went wrong in loading the image
+    if( loadedImage != NULL )
+    {
+        //Create an optimized image
+        optimizedImage = SDL_DisplayFormat( loadedImage );
 
-
-void test_display_board(void)
-{
-	int i = 0; 
-	int j = 0; 
-
-	for (i = 0; i < 3; i++) 
-	{
-		printf(" \n ");
-		for (j = 0; j < 3; j++) 
-		{
-			printf(" %c ", board[i][j]); 
-		}
-	}
-	printf(" \n "); 
-	return 0;
+        //Free the old image
+        SDL_FreeSurface( loadedImage );
+    }
+     //Return the optimized image
+    return optimizedImage;
 }
 
 
 
-
-void test_input(void) 
+int main(int argc, char *argv[])
 {
-	int row_or_column = 0; 
-	char* string_to_print[7]; 
-	if (number == 0) 
-	{
-		*string_to_print = "row";
-	} else if (number == 1) 
-	{
-		*string_to_print = "column";
-	} else {
-		printf("\n This is not supposed to hapen. \n"); 
-	}
-	int valid = 0; 
-	while (valid == 0)
-	{
-		printf("\n Choose a %s> ", *string_to_print); 
-		scanf("%d", &row_or_column);  
-		if (1 <= row_or_column && row_or_column <= 3)
-		{
-			printf("\n You have chosen %s #%d. \n", *string_to_print, row_or_column);
-			valid = 1;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Surface* screen;
+    screen=SDL_SetVideoMode(500,350,32,SDL_SWSURFACE);
+    SDL_Surface* bg=load_image("dark.jpg");
+    int key_press;
+    SDL_WM_SetIcon(IMG_Load("pacman_16X16.png"), NULL);
+    SDL_WM_SetCaption("Pacman","pacman_16X16.png");
 
-		} else {
-			printf("\n %s = %d \n", *string_to_print, row_or_column); 
-			printf("\n Invalid %s chosen, please try again. \n (The %s number must be either 1, 2, or 3.) \n", *string_to_print, *string_to_print);
+    SDL_Event event;
+    extern int quit;
 
-		}
-	}
-	return row_or_column;
+    SDL_BlitSurface(bg,NULL,screen,NULL);
+  // build_map(screen);
+    gameplay(screen);
+
+
+
+    return 0;
 }
 
 
 
 
-void test_winner(void) 
-{
-	int winner_number = 0;
-	int blank = 0;
-	int i = 0; 
-	int j = 0; 
-	if (((board[0][0] == 'X') && (board[0][1] == 'X') && (board[0][2] == 'X')) || // first row
-		((board[0][0] == 'X') && (board[1][0] == 'X') && (board[2][0] == 'X')) || // first column
-		((board[0][0] == 'X') && (board[1][1] == 'X') && (board[2][2] == 'X')) || // diagonal 1
-		((board[1][0] == 'X') && (board[1][1] == 'X') && (board[1][2] == 'X')) || // second row
-		((board[2][0] == 'X') && (board[2][1] == 'X') && (board[2][2] == 'X')) || // third row
-		((board[0][1] == 'X') && (board[1][1] == 'X') && (board[2][1] == 'X')) || // second column
-		((board[0][2] == 'X') && (board[1][2] == 'X') && (board[2][2] == 'X')) || // third column
-		((board[0][2] == 'X') && (board[1][1] == 'X') && (board[2][0] == 'X')) // diagonal 2
-		)
-	{
-		winner_number = 1;
-	} else if (((board[0][0] == 'O') && (board[0][1] == 'O') && (board[0][2] == 'O')) || // first row
-			   ((board[0][0] == 'O') && (board[1][0] == 'O') && (board[2][0] == 'O')) || // first column
-			   ((board[0][0] == 'O') && (board[1][1] == 'O') && (board[2][2] == 'O')) || // diagonal 1
-			   ((board[1][0] == 'O') && (board[1][1] == 'O') && (board[1][2] == 'O')) || // second row
-			   ((board[2][0] == 'O') && (board[2][1] == 'O') && (board[2][2] == 'O')) || // third row
-			   ((board[0][1] == 'O') && (board[1][1] == 'O') && (board[2][1] == 'O')) || // second column
-			   ((board[0][2] == 'O') && (board[1][2] == 'O') && (board[2][2] == 'O')) || // third column
-			   ((board[0][2] == 'O') && (board[1][1] == 'O') && (board[2][0] == 'O')) // diagonal 2
-			   )
-	{
-		winner_number = 2;
-	} else {
-		for (i = 0; i < 3; i++) 
-		{
-			for (j = 0; j < 3; j++) 
-			{
-				if (board[i][j] == '_') {
-					blank = 1; 
-				}
-			}
-		}
-		if (blank == 0) { 
-			winner_number = 3;
-		}
-	}
 
-	return winner_number;
+
+void pacman_sprites(SDL_Rect location,int direction,SDL_Surface* screen,char comp[20][12])
+{
+
+
+
+    SDL_Rect char_up,char_down,char_left,char_right,char_neutral,char_dead;
+//definition of sprites
+    char_up.x=0; char_up.y=20;  char_up.w=20;  char_up.h=20;
+    char_down.x=20; char_down.y=20;  char_down.w=20;  char_down.h=20;
+    char_left.x=20; char_left.y=0;  char_left.w=20;  char_left.h=20;
+    char_right.x=40; char_right.y=0;  char_right.w=20;  char_right.h=20;
+    char_neutral.x=0; char_neutral.y=0;  char_neutral.w=20;  char_neutral.h=20;
+    char_dead.x=40; char_dead.y=20;  char_dead.w=20;  char_dead.h=20;
+// end def of sprites
+    SDL_Surface *one,*two;
+    one=load_image("pacman.gif");
+    two=load_image("pacman_follow.gif");
+
+    if  (legibility(comp,&direction)==yes)
+    {
+        moveit(direction); // it just changes  the co-ordinates.....doesnt animate sprites
+    switch(direction)
+    {
+        case SDLK_UP:
+        SDL_BlitSurface(one,&char_up,screen,&location);
+        SDL_Flip(screen);
+        break;
+        case SDLK_DOWN:
+        SDL_BlitSurface(one,&char_down,screen,&location);
+        SDL_Flip(screen);
+         break;
+        case SDLK_RIGHT:
+        SDL_BlitSurface(one,&char_right,screen,&location);
+        SDL_Flip(screen);
+        break;
+        case SDLK_LEFT:
+        SDL_BlitSurface(one,&char_left,screen,&location);
+        SDL_Flip(screen);
+        break;
+    }
+
+
+    SDL_Delay(75);
+    SDL_BlitSurface(one,&char_neutral,screen,&location);
+    SDL_Flip(screen);
+
+
+    SDL_Delay(75);
+    SDL_BlitSurface(two,NULL,screen,&location);
+    SDL_Flip(screen);
+    }
+    else
+    {
+        //SDL_Delay(1000);
+        SDL_BlitSurface(one,&char_neutral,screen,&location);
+        SDL_Flip(screen);
+    }
+
+
+
+    SDL_FreeSurface(one);
+    SDL_FreeSurface(two);
+
 }
+
+
+
+void gameplay(SDL_Surface* screen)
+{
+
+    extern int quit;
+    char comp[20][12];
+    extern SDL_Rect cor;
+    SDL_Event event;
+    int key_press;
+
+
+
+    createmap(screen,comp);
+
+    while (quit==no)
+    {
+           while(SDL_PollEvent(&event))
+           {
+
+                if(event.type==SDL_QUIT) quit=yes;
+                if (event.type==SDL_KEYDOWN)   key_press=event.key.keysym.sym;
+            }
+
+
+
+
+            pacman_sprites(cor,key_press,screen,comp);
+
+    }
+}
+
 
 
 
